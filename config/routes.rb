2a8823be7c0ab977a243b76fa91dd0c1b4a1da4e
charlesagyemang <micnkru@gmail.com>
2022-02-13 12:44:52 +0000

    push shirt

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'admin/dashboard'
   resources :signboards
   resources :owners
-  root to: 'signboards#index'
+  root to: 'owners#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
