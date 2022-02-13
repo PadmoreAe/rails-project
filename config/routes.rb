d3ca2149b64842c8a73285ac16f649b70825b131
charlesagyemang <micnkru@gmail.com>
2022-02-13 12:39:42 +0000

    make search work

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'admin/dashboard'
   resources :signboards
   resources :owners
+  root to: 'signboards#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
